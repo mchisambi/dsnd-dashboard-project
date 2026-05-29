fa0fe3def7cb00b778c26a66b66eaae52be84796
mchisambi <mchisambi@gmail.com>
2026-05-29 11:08:29 +0100

    Updated both files

--- python-package/employee_events/employee.py
+++ python-package/employee_events/employee.py
@@ -1,13 +1,21 @@
 # Import the QueryBase class
 #### YOUR CODE HERE
+from oauthlib.uri_validate import query
+
+from .query_base import QueryBase
 
 # Import dependencies needed for sql execution
 # from the `sql_execution` module
 #### YOUR CODE HERE
 
+
 # Define a subclass of QueryBase
 # called Employee
 #### YOUR CODE HERE
+
+class Employee(QueryBase):
+    name = "employee"
+    id_check = "employee_id"
 
     # Set the class attribute `name`
     # to the string "employee"
@@ -19,7 +27,9 @@
     # This method should return a list of tuples
     # from an sql execution
     #### YOUR CODE HERE
-        
+    def names(self):
+        return self.query(f"SELECT first_name, last_name FROM {self.name}")
+
         # Query 3
         # Write an SQL query
         # that selects two columns 
@@ -28,14 +38,18 @@
         # This query should return the data
         # for all employees in the database
         #### YOUR CODE HERE
-    
+
+    def name_id(self):
+        return self.pandas_query(f"SELECT first_name || ' ' || last_name AS full_name , employee_id FROM {self.name}")
 
     # Define a method called `username`
     # that receives an `id` argument
     # This method should return a list of tuples
     # from an sql execution
     #### YOUR CODE HERE
-        
+    def username(self, id):
+        return self.query(f"SELECT first_name || ' ' || last_name AS username , employee_id FROM {self.name} WHERE {self.id_check} = {id}")
+
         # Query 4
         # Write an SQL query
         # that selects an employees full name
@@ -43,6 +57,7 @@
         # to only return the full name of the employee
         # with an id equal to the id argument
         #### YOUR CODE HERE
+
 
 
     # Below is method with an SQL query
@@ -55,11 +70,11 @@
     #### YOUR CODE HERE
     def model_data(self, id):
 
-        return f"""
+        return self.pandas_query(f"""
                     SELECT SUM(positive_events) positive_events
                          , SUM(negative_events) negative_events
                     FROM {self.name}
                     JOIN employee_events
                         USING({self.name}_id)
                     WHERE {self.name}.{self.name}_id = {id}
-                """
+                """)
